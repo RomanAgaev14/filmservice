226dac802100d8b60f59f689cba25226b3f20968
RomanAgaev14 <98325616+RomanAgaev14@users.noreply.github.com>
2022-04-21 19:00:48 +0300

    Add files via upload

--- filmoteka/src/main/java/com/example/filmoteka/FilmotekaApplication.java
+++ filmoteka/src/main/java/com/example/filmoteka/FilmotekaApplication.java
@@ -1,9 +1,11 @@
 package com.example.filmoteka;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class FilmotekaApplication {
 
 	public static void main(String[] args) {
